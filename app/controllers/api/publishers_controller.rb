293e9419c5395a16592a055430dd13dea4c7aef8
miranser <miranserandroid@gmail.com>
2019-01-11 22:21:45 +0300

    Add POST API endpoits to models

--- app/controllers/api/publishers_controller.rb
+++ app/controllers/api/publishers_controller.rb
@@ -1,14 +1,32 @@
 module Api
     class PublishersController < ApplicationController
+        before_action :set_publisher, only: [:show, :update]
+        
         def index
             @publishers = Publisher.all
             render json: @publishers, each_serializer: PublisherSerializer
         end
         
         def show
-            @publisher = Publisher.find(params[:id])
             render json: @publisher, serializer: PublisherSerializer
         end
 
+        def create
+            @publisher = Publisher.new(publisher_params)
+            if @publisher.save
+                render json: @publisher, serializer: PublisherSerializer
+            else
+                render json: {status: "ERROR", message: "Publisher is not saved", data: @publisher.errors}
+            end
+        end
+
+        private
+        def set_publisher
+            @publisher = Publisher.find(params[:id])    
+        end
+        
+        def publisher_params
+            params.permit(:name)    
+        end
     end
 end
--- app/controllers/api/shops_controller.rb
+++ app/controllers/api/shops_controller.rb
@@ -1,14 +1,32 @@
 module Api
     class ShopsController < ApplicationController
+        before_action :set_shop
         def index
             @shops = Shop.all
             render json: @shops, each_serializer: ShopSerializer
         end
         
         def show
-            @shop = Shop.find(params[:id])
             render json: @shop, serializer: ShopSerializer
         end
 
+        def create
+            @shop = Shop.new(shop_params)
+            if @shop.save
+                render json: @shop, serializer: ShopSerializer
+            else
+                render json: {status: "ERROR", message: "Shop is not saved", data: @shop.errors}
+            end
+        end
+
+        private
+
+        def set_shop
+            @shop = Shop.find(params[:id])    
+        end
+
+        def shop_params
+            params.permit(:name)
+        end
     end
 end
